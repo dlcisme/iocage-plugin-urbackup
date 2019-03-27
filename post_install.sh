41a60e8d536b1d1279f509bd447f9855ee4e280c
dlcisme <34501019+dlcisme@users.noreply.github.com>
2019-03-26 21:00:17 -0600

    add server start flags

--- post_install.sh
+++ post_install.sh
@@ -12,5 +12,8 @@ chown -R urbackup:urbackup $DATA_LOCATION
 # enable UrBackup to start at boot
 sysrc "urbackup_server_enable=YES"
 
+# set Nginx config file and data directory location
+sysrc "urbackup_server_flags=-c $DATA_LOCATION/urbackupsrv.conf"
+
 # start the urbackup service
 # service urbackup start
